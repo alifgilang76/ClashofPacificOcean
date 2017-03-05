32343d56d0cee02d0717c6132279a7fcc08418cc
silverserpent9 <alifgilang76@gmail.com>
2017-03-05 14:51:13 +0700

    Add files via upload

---
+++ Assets/mainmenu.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour {
+
+	public GameObject StartBtn, SubMenu, AboutPanel, ProloguePanel, BattleBtn;
+	public Text PrologueText;
+	public string PrologueText_tmp;
+	public float letterPause;
+
+	void Start(){
+		PrologueText_tmp = PrologueText.text;
+		PrologueText.text = "";
+	}
+
+	public void ChooseMenu (Button btn){
+		switch (btn.name) {
+		case "Start Button":
+			StartBtn.SetActive (false);
+			SubMenu.SetActive (true);
+			break;
+		case "New Game":
+			ProloguePanel.SetActive (true);
+			StartCoroutine ("TypeText");
+
+			break;
+		case "Battle Button":
+			SceneManager.LoadSceneAsync ("Map", LoadSceneMode.Single);
+			break;
+		case "About Game":
+			AboutPanel.SetActive (true);
+			SubMenu.SetActive (false);
+			break;
+		case "Exit About":
+			AboutPanel.SetActive (false);
+			SubMenu.SetActive (true);
+			break;
+		case "Exit":
+			Application.Quit ();
+			break;
+		}
+	}
+
+	IEnumerator TypeText () {
+		foreach (char letter in PrologueText_tmp.ToCharArray()) {
+			PrologueText.text += letter;
+			yield return 0;
+			yield return new WaitForSeconds (letterPause);
+		}
+		BattleBtn.SetActive (true);
+	}
+}
---
+++ Assets/pauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour {
+
+	public ResGUI getResGUI;
+	public GameObject PauseMenu;
+
+	public void Confirm(Button YesNo){
+		switch (YesNo.name) {
+		case ("Yes"):
+			Application.Quit();
+			break;
+		case ("No"):
+			PauseMenu.SetActive (false);
+			getResGUI.isPause = false;
+			Time.timeScale = 1;
+			break;
+		}
+	}
+	public void BackToMainMenu(){
+		SceneManager.LoadSceneAsync ("Main_Menu", LoadSceneMode.Single);
+		Time.timeScale = 1;
+	}
+
+}
